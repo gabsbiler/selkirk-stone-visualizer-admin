f8d90d1659f0fc55f64239209a46322e2a55f688
Gabriel Biler <gabsbiler@gmail.com>
2024-02-03 00:28:25 +0800

    fix routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from '~pages'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     ...setupLayouts(routes),
   ],
