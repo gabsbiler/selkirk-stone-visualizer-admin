125bb69f1bf313672bc4aa04a5396ce0b0600f89
Gabriel Biler <gabsbiler@gmail.com>
2024-03-14 00:02:05 +0800

    minor bug fix

--- src/pages/pages/content/index.vue
+++ src/pages/pages/content/index.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import Footer from '@/layouts/components/content/Footer.vue'
+import Section1 from '@/layouts/components/content/Section1.vue'
+import Section2 from '@/layouts/components/content/Section2.vue'
 import Section3 from '@/layouts/components/content/Section3.vue'
+import Section4 from '@/layouts/components/content/Section4.vue'
 </script>
 
 <template>
