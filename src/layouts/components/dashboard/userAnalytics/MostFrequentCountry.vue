688aff38b99117070baf0f9768e74576b2197e37
Genrev Gabriel Rodriguez <genrevgrodriguez@gmail.com>
2024-04-06 00:44:49 +0800

    Implemented user analytics

--- src/layouts/components/dashboard/userAnalytics/MostFrequentCountry.vue
+++ src/layouts/components/dashboard/userAnalytics/MostFrequentCountry.vue
@@ -1,22 +1,27 @@
 <script setup lang="ts">
-const items = ref([
-  {
-    country: 'Nigeria',
-    count: '85k',
-  },
-  {
-    country: 'Philippines',
-    count: '85k',
-  },
-  {
-    country: ' Canada',
-    count: '85k',
-  },
-  {
-    country: 'Taiwan',
-    count: '85k',
-  },
-])
+const props = defineProps<{
+  data: object
+}>()
+
+const { data } = toRefs(props)
+
+watchDebounced(data, () => {
+  const value = data.value || []
+  const newItems = []
+
+  for (let i = 0; i < Math.min(10, value.length); i++) {
+    const item = value[i]
+
+    newItems.push({
+      country: item.ClientCountryOrRegion,
+      count: item.count_,
+    })
+  }
+
+  items.value = newItems
+}, { debounce: 100, maxWait: 200 })
+
+const items = ref([])
 </script>
 
 <template>
@@ -26,25 +31,6 @@ const items = ref([
         Most Frequent Users Location
       </h6>
       <hr class="my-3">
-      <div class="d-flex  justify-space-between">
-        <div
-          class="d-flex align-center gap-x-2"
-          style="min-inline-size: 12rem;"
-        >
-          Filter By
-          <VSelect
-            label="Select"
-            :items="['User', 'IP Address']"
-            density="compact"
-          />
-        </div>
-        <div style="min-inline-size: 10rem;">
-          <VTextField
-            label="Search"
-            density="compact"
-          />
-        </div>
-      </div>
       <div>
         <VTable>
           <thead>
