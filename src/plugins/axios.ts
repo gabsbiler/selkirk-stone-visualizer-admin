6fd598a5b1cc04e1270be2db52692d468c2fdfb8
Genrev Gabriel Rodriguez <genrevgrodriguez@gmail.com>
2024-05-13 19:17:57 +0800

    Updated api url to staging

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'https://api.selkirkstone.app/api/',
+  baseURL: 'https://selkirkappapi-staging.azurewebsites.net/api/',
 
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
