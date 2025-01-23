281069adc7fd2915c86ac6213b332b4a22075e8e
Genrev Gabriel Rodriguez <genrevgrodriguez@gmail.com>
2025-01-24 00:55:43 +0800

    Fixed issue

--- src/layouts/components/dashboard/summary/PopularityRanking.vue
+++ src/layouts/components/dashboard/summary/PopularityRanking.vue
@@ -68,20 +68,25 @@ const formatNumber = num => {
         :headers="headers"
         items-per-page="5"
       >
-        <template v-slot:item.visits="{ value }">
-          {{ formatNumber(value) }}
-        </template>
-
-        <template v-slot:item.dataInPercentage="{ value }">
-          <VProgressLinear
-            v-model="value"
-            color="primary"
-            height="20"
-          >
-            <template #default="{ value }">
-              {{ Math.ceil(value) }}%
-            </template>
-          </VProgressLinear>
+        <template v-slot:item="{ item }">
+          <tr>
+            <td>{{ item.stoneVariant }}</td>
+            <td>{{ item.location }}</td>
+            <td>
+              {{ formatNumber(item.visits) }}
+            </td>
+            <td>
+              <VProgressLinear
+                v-model="item.dataInPercentage"
+                color="primary"
+                height="20"
+              >
+                <template #default="{ value }">
+                  {{ Math.ceil(value) }}%
+                </template>
+              </VProgressLinear>
+            </td>
+          </tr>
         </template>
       </VDataTable>
     </VCardText>
